e071c49f520919f70b6f3b7e54079a178853924d
jian <loveinansan1@gmail.com>
2024-12-06 13:10:54 +0900

    #4 inflearn_springMVC1
    class3_Hello 서블릿

---
+++ src/test/java/hello/servlet/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

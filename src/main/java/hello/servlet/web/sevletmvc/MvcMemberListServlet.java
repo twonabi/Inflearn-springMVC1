fb265f807affb77b6df3a9fcf642e0a82e8d0b84
jian <loveinansan1@gmail.com>
2024-12-20 14:26:55 +0900

    #29 inflearn_springMVC1
    class4_MVC 패턴_회원등록 폼_회원 목록 조회_뷰

---
+++ src/main/java/hello/servlet/web/sevletmvc/MvcMemberListServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.web.sevletmvc;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MvcMemberListServlet service() 호출됨"); // 디버깅 로그
+        List<Member> members = memberRepository.findAll();
+        System.out.println("members = " + members); // 데이터 확인 로그
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}

0ae6be006e55d619e13def43913d9177307afebc
jian <loveinansan1@gmail.com>
2024-12-20 14:27:51 +0900

    #29 inflearn_springMVC1
    class4_MVC 패턴_회원등록 폼_회원 목록 조회_컨트롤러

--- src/main/java/hello/servlet/web/sevletmvc/MvcMemberListServlet.java
+++ src/main/java/hello/servlet/web/sevletmvc/MvcMemberListServlet.java
@@ -19,9 +19,9 @@ public class MvcMemberListServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("MvcMemberListServlet service() 호출됨"); // 디버깅 로그
+
         List<Member> members = memberRepository.findAll();
-        System.out.println("members = " + members); // 데이터 확인 로그
+
         request.setAttribute("members", members);
 
         String viewPath = "/WEB-INF/views/members.jsp";

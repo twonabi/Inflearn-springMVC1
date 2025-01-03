8a788ccfd1a21aeeda2db1fc95fad4447d97e5fa
jian <loveinansan1@gmail.com>
2025-01-03 12:14:41 +0900

    #46 inflearn_springMVC1
    class5_MVC 프레임워크 만들기_Model 추가_V3_ModelView_오타 수정

--- src/main/java/hello/servlet/web/frontcontroller/ModelView.java
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 
 public class ModelView {
     private String viewName;
-    private Map<String, Objects> model = new HashMap<>();
+    private Map<String, Object> model = new HashMap<>();
 
     public ModelView(String viewName) {
         this.viewName = viewName;
@@ -20,11 +20,11 @@ public class ModelView {
         this.viewName = viewName;
     }
 
-    public Map<String, Objects> getModel() {
+    public Map<String, Object> getModel() {
         return model;
     }
 
-    public void setModel(Map<String, Objects> model) {
+    public void setModel(Map<String, Object> model) {
         this.model = model;
     }
 }

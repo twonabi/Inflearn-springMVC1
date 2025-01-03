3f4d2d42e8e6da2f5bb9a582e6093f0f27dcc8a2
jian <loveinansan1@gmail.com>
2025-01-03 12:50:48 +0900

    #50 inflearn_springMVC1
    class5_MVC 프레임워크 만들기_Model 추가_V3_MemberListControllerV3 오타 수정

--- src/main/java/hello/servlet/web/frontcontroller/V3/controller/MemberListControllerV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/V3/controller/MemberListControllerV3.java
@@ -16,7 +16,7 @@ public class MemberListControllerV3 implements ControllerV3 {
     public ModelView process(Map<String, String> paramMap) {
         List<Member> members = memberRepository.findAll();
         ModelView mv = new ModelView("members");
-        mv.getModel().put("member", members);
+        mv.getModel().put("members", members);
 
         return mv;
     }

995d9a5b86ddf637997e3c6370b376a26dbbb358
jian <loveinansan1@gmail.com>
2025-01-08 16:18:48 +0900

    #59 inflearn_springMVC1
    class5_MVC 프레임워크_유연한 컨트롤러1_v5_ControllerV3HandlerAdapter2
    
    어뎁터 패턴

--- src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import javax.imageio.IIOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
     @Override
@@ -18,6 +20,17 @@ public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
     @Override
     public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IIOException {
         ControllerV3 controller = (ControllerV3) handler;
-        return null;
+
+        Map<String, String> paramMap = createParamMap(request);
+        ModelView mv = controller.process(paramMap);
+
+        return mv;
+    }
+
+    private Map<String ,String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
     }
 }
